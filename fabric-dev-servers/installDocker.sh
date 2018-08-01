c5677e87d963ee962180a5e097de06b21b26d823
tu@agiletech.vn <tubackkhoa@gmail.com>
2018-08-01 13:55:20 +0700

    check docker before download fabric

---
+++ fabric-dev-servers/installDocker.sh
@@ -0,0 +1,23 @@
+# docker ce
+if [ ! `command -v docker` ];then
+
+  echo 'Install docker...'
+  
+  if [ -n "$(command -v apt)" ];then  
+    sudo apt install docker.io
+  else 
+    sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    sudo yum makecache fast
+    sudo yum install docker-ce -y
+  fi
+
+  echo "install docker-compose"
+  sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+
+  sudo systemctl start docker
+  sudo systemctl enable docker
+else 
+  docker --version 
+fi
